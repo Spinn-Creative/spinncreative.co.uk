d735f90b3499ddc38cdb264ff65bad98116c4bab
wsscuid798 <173471619+wsscuid798@users.noreply.github.com>
2024-06-29 14:29:45 +0100

    Update code base

--- app/layout.js
+++ app/layout.js
@@ -45,6 +45,16 @@ export default function RootLayout({ children }) {
         `}
       </Script>
 
+      <Script id="ms-clarity">
+        {`
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "mzb27qfp9g");
+        `}
+</Script>
+
         <div className="px-2">
           <Header role='header' />
             <main className="sm:container" role="main">{children}</main>
