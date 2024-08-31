e4b836c6e0455cdd052d7c9328c61d7870b009e6
wsscuid798 <173471619+wsscuid798@users.noreply.github.com>
2024-08-31 19:05:46 +0100

    Update codebase

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ The Node.js runtime (version 22+).
 
 First, run the development server:
 
-```bashh
+```bash
 npm run dev
 # or
 yarn dev
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,29 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      'sc-tan': '#dac8be',
+      'sc-brown': '#55433a',
+      'sc-orange': '#ff856b',
+      'sc-green': '#7fd1ae',
     },
+    fontFamily: {
+      sans: ['Inter', 'Arial', 'san-serif'],
+      serif: ['serif'],
+    },
+    extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/container-queries'),
+  ],
 };
 export default config;
