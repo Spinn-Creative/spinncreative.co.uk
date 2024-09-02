df6587d22f5c5827e0f4f4c0dbe0da6954b11b30
wsscuid798 <173471619+wsscuid798@users.noreply.github.com>
2024-09-02 23:01:17 +0100

    Fix: Update navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Navbar from '@/components/Navbar';
 export default function Header() {
     return (
       <header className='header'>
-        <div className=''>
+        <div className='md:container'>
           <Navbar />
         </div>
       </header>
